ab360932e7c0666ff4cc38d8619827228504e19d
Alexander DeRieux <alexander.derieux@gmail.com>
2022-10-09 19:03:33 -0400

    New page header
    - Tab title is now group name
    - Uses group logo as tab icon
    - References Bootstrap styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+    <>
+        <Head>
+            <title>NEWS@VT</title>
+            <meta name="description" content="NEWS@VT website" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/newsvticon.png" />
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"/>
+        </Head>
+
+        <Component {...pageProps} />
+
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"/>
+    </>
+    );
 }
 
 export default MyApp
