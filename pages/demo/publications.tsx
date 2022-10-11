9daa5328e28d3b79df6287054b1a22f618a73724
Alexander DeRieux <alexander.derieux@gmail.com>
2022-10-11 13:13:02 -0400

    New bar plot for cited numbers

--- pages/demo/publications.tsx
+++ pages/demo/publications.tsx
@@ -1,5 +1,26 @@
 import { useEffect, useState } from "react";
 import { Article, fetchAuthorProfileData, ProfileData } from "../../lib/google-scholar";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+  
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 
 type PublicationsDemoProps = {
@@ -18,6 +39,48 @@ export default function PublicationsDemo({ authorProfileData }: PublicationsDemo
     //     getAuthorProfileData();
     // }, []);
 
+    const chart_options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top' as const,
+            display: false,
+          },
+          title: {
+            display: true,
+            text: 'Cited by',
+          },
+        },
+      };
+    const chart_labels = authorProfileData.cited_by.graph.map((item, index) => item.year)
+    const chart_data = {
+        labels: chart_labels,
+        datasets: [{
+            label: "Cited By",
+            // display: false,
+            data: authorProfileData.cited_by.graph.map((item, index) => item.citations),
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1,
+        }],
+    };
+
+    console.log(JSON.stringify(chart_data, null, 4))
+
     const renderArticle = (article: Article, key: any) => {
         return (<a key={key} href={article.link} className="list-group-item list-group-item-action flex-column align-items-start">
             <div className="d-flex w-100 justify-content-between">
@@ -30,12 +93,18 @@ export default function PublicationsDemo({ authorProfileData }: PublicationsDemo
     }
 
     return (
-        <div className="m-5">
+        <>
+        <div>
+            <h1>Citation Statistics</h1>
+            <Bar data={chart_data} options={chart_options}/>
+        </div>
+        <div>
             <h1>Google Scholar Articles for {authorProfileData?.author.name} ({authorProfileData?.articles.length} total)</h1>
             <div className="list-group">
                 {authorProfileData?.articles.map((article, index) => renderArticle(article, index))}
             </div>
         </div>
+        </>
     );
 }
 
