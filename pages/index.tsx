86ed1c31777e9e3aeef235beac3fcdc3d9bfcdca
Alexander DeRieux <alexander.derieux@gmail.com>
2022-10-10 23:33:24 -0400

    Add test alert

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,15 @@ import type { NextPage } from 'next'
 import Home from '../components/home.mdx';
 
 const HomePage: NextPage = () => {
+
+    const onClick = () => {
+        alert("This is an alert");
+    }
+
     return (
         <div className="container-sm p-5">
             <Home />
+            <button className="btn btn-success" onClick={ onClick }>Test Alert</button>
         </div>
     )
 }
